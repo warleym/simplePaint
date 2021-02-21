5575895fadcfc4c276cd2b811b68639678a72960
Warley Máximo de Matos Pereira <warleym.matos@gmail.com>
2021-02-21 12:18:46 -0300

    Aplicação desenho livre OK

--- app/src/main/java/br/com/progiv/simplepaint/MainActivity.java
+++ app/src/main/java/br/com/progiv/simplepaint/MainActivity.java
@@ -3,16 +3,22 @@ package br.com.progiv.simplepaint;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewCanvas viewCanvas;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
-//
+        viewCanvas = findViewById(R.id.area_desenho);
+    }
 
+    public void lpimparDesenho(View view) {
+        viewCanvas.limparCanvas();
     }
 }
--- app/src/main/java/br/com/progiv/simplepaint/ViewCanvas.java
+++ app/src/main/java/br/com/progiv/simplepaint/ViewCanvas.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Path;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -14,10 +15,80 @@ public class ViewCanvas extends View {
     private Path path;
     private Bitmap bitmap;
     private Canvas canvas;
-    private float fixoX, fixoY;
+    private float eixoX, eixoY;
     private int TOLERANCIA_MOVIMENTO = 5;
+    private Linha linha;
 
     public ViewCanvas(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        inicializaObjetos();
+    }
+
+    //inicializar os objetos
+    private void inicializaObjetos() {
+        path = new Path();
+        linha = new Linha(getContext(), path);
+    }
+
+    //controlar o início do toque
+    private void inicioToque(float x, float y) {
+        path.moveTo(x, y);
+        eixoX = x;
+        eixoY = y;
+    }
+
+    //controlatr movimento
+    private void moveuDedoTela(float x, float y) {
+        float distanciaX = Math.abs(x - eixoY);
+        float distanciaY = Math.abs(y - eixoY);
+        if (distanciaX >= TOLERANCIA_MOVIMENTO || distanciaY >= TOLERANCIA_MOVIMENTO) {
+            path.quadTo(eixoX, eixoY, (x + eixoX) / 2, (y + eixoY) / 2);
+        }
+        eixoX = x;
+        eixoY = y;
+    }
+
+    //controlar o final do toque
+    private void tirouDedoTela() {
+        path.lineTo(eixoX, eixoY);
+    }
+
+    //controlar a limpeza da tela
+    public void limparCanvas() {
+        path.reset();
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        linha.desenharLinha(canvas);
+    }
+
+    @Override protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(bitmap);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+            float x = event.getX();
+            float y = event.getY();
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    inicioToque(x, y);
+                    invalidate();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    moveuDedoTela(x, y);
+                    invalidate();
+                    break;
+                case MotionEvent.ACTION_UP:
+                    tirouDedoTela();
+                    invalidate();
+                    break;
+            }
+            return true;
     }
 }
